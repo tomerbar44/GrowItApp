7f57e7393d537833b03ba697b92047458d0dd87b
Tal Adivi <39825719+TalAdivi@users.noreply.github.com>
2020-07-17 11:33:27 +0300

    some testubg changes

--- GrowIt - test/__tests__/MainScreen.test.js
+++ GrowIt - test/__tests__/MainScreen.test.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { render } from '@testing-library/react-native'
-import MainScreen from '../src/components/MainScreen/MainScreen'
+// import React from 'react'
+// import { render } from '@testing-library/react-native'
+// import MainScreen from '../src/components/MainScreen/MainScreen'
 
-describe('MainScreen component Test', function () {
+// describe('MainScreen component Test', function () {
    
-    test('render main screen component snapshot ', function () {
-      const { baseElement }= render(<MainScreen/>)
-      expect(baseElement).toMatchSnapshot()
-    });
+//     test('render main screen component snapshot ', function () {
+//       const { baseElement }= render(<MainScreen/>)
+//       expect(baseElement).toMatchSnapshot()
+//     });
   
-});
+// });
 
 
 
